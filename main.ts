c72d1a46b0cc15535fe7314be35e7873ffda03ac
baa <34665120+baa-mb@users.noreply.github.com>
2024-11-04 15:05:28 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,42 @@
-basic.showIcon(IconNames.Yes)
+input.onButtonPressed(Button.A, function () {
+    automat = 1
+})
+input.onButtonPressed(Button.B, function () {
+    automat = 0
+})
+let automat = 0
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . . . .
+    `)
+automat = 0
 basic.forever(function () {
-    pins.servoWritePin(AnalogPin.P0, 180)
-    basic.pause(1000)
-    pins.servoWritePin(AnalogPin.P0, 0)
-    basic.pause(1000)
+    if (automat) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . . . .
+            . # . . .
+            . . # . .
+            `)
+        for (let i = 0; i <= 6; i++) {
+            pins.servoWritePin(AnalogPin.P0, 180 - 30 * i)
+            basic.pause(1000)
+        }
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            . . . . #
+            . . . # .
+            . . # . .
+            `)
+        for (let i = 0; i <= 6; i++) {
+            pins.servoWritePin(AnalogPin.P1, 180 - 30 * i)
+            basic.pause(2000)
+        }
+    }
+    automat = 0
 })
