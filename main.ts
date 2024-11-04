31ca13721708d87fc01d1f81534eba40895638ad
baa <34665120+baa-mb@users.noreply.github.com>
2024-11-04 14:39:02 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.showIcon(IconNames.Yes)
+basic.forever(function () {
+    pins.servoWritePin(AnalogPin.P0, 180)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P0, 0)
+    basic.pause(1000)
+})
