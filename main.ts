1560fcf7387aa126c5e5c5c49d6f7e689b6fc1c0
baa <34665120+baa-mb@users.noreply.github.com>
2024-11-04 16:07:18 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ input.onButtonPressed(Button.A, function () {
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "A") {
+        basic.showString("A")
         radio.raiseEvent(
         EventBusSource.MICROBIT_ID_BUTTON_A,
         EventBusValue.MICROBIT_BUTTON_EVT_CLICK
